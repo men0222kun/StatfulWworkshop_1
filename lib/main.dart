1336187eaa367fd1b8f5e5dcccbe292486e02dd0
men0222kun <98504220+men0222kun@users.noreply.github.com>
2022-08-18 16:14:14 +0700

    up stateFul

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
-//import 'package:myfirstapp/Counter.dart';
-import 'package:myfirstapp/Design_layout.dart';
-import 'package:myfirstapp/Workshop_Airbnb.dart';
 
-import 'Workshop_design.dart';
-// import 'package:myfirstapp/Workshop_staful.dart';
-// import 'package:myfirstapp/work1_ProfileScr.dart';
-// import 'package:myfirstapp/Test1_basic.dart';
-
-// import 'Layout_2.dart';
-
-
+import 'package:myfirstapp/attraction_before.dart';
 void main() {
   runApp(MyApp());
 }
@@ -23,44 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Airbnb(),
+      home: Attraction(),
       //theme: ThemeData(primarySwatch: Colors.blueGrey),
       debugShowCheckedModeBanner: false,
       
     );
   }
 }
-/*
-class Myhomepage extends StatefulWidget {
-  // ignore: prefer_const_constructors_in_immutables
-  Myhomepage({Key? key}) : super(key: key);
-
-  @override
-  State<Myhomepage> createState() => _MyhomepageState();
-}
-
-class _MyhomepageState extends State<Myhomepage> {
-  int number = 0; //การสร้าง State
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network('https://media.istockphoto.com/photos/lion-cub-sitting-looking-at-the-camera-7-weeks-old-picture-id464621461?k=20&m=464621461&s=612x612&w=0&h=EPR6RsOC7zj1KkY6Ghsb0_5qX6G0UOXyppi0bK1joTg='),
-            Text("Thitipat Saramd"),
-
-          ],
-        ),
-      ),
-    );
-  }
-
-
-}
-*/
